e0253a996222acf322b5b6c9b7cfd23b78e3b1b4
Tushar Pathak <97155038+FieryGhoul@users.noreply.github.com>
2025-04-03 22:35:43 +0530

    Create person.js

---
+++ models/person.js
@@ -0,0 +1,24 @@
+// models/person.js
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['Male', 'Female', 'Other']
+  },
+  mobileNumber: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Person', personSchema);
